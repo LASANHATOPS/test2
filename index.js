deb9acde3e689d3849d29c73170526bf09c1c1b1
LASANHATOPS <lasanhatops@gmail.com>
2026-05-16 18:34:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const PORT = process.env.PORT || 3000;
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 const ALLOWED_CHANNEL_ID = process.env.ALLOWED_CHANNEL_ID;
+process.on("unhandledRejection", console.error);
+process.on("uncaughtException", console.error);
